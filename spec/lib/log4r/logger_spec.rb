542764d6ddbd727e545f2645b8bf3d23d086664a
Rob Hanlon <rob@mediapiston.com>
2011-10-17 16:41:27 -0700

    Refactor host into let.

--- spec/lib/log4r/logger_spec.rb
+++ spec/lib/log4r/logger_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe Log4r::Logger do
   describe 'with RemoteSyslogOutputter' do
+    let(:host) { 'localhost' }
     let(:port) { rand(65535 - 1024) + 1024 }
-    let(:outputter) { Log4r::RemoteSyslogOutputter.new('outputter', '127.0.0.1', port) }
+    let(:outputter) { Log4r::RemoteSyslogOutputter.new('outputter', host, port) }
     let(:socket) { UDPSocket.new }
 
     subject { Log4r::Logger.new('Logger') }
 
     before(:each) do
       subject.outputters << outputter
-      socket.bind('127.0.0.1', port)
+      socket.bind(host, port)
     end
 
     it 'should make a UDP request to localhost' do
