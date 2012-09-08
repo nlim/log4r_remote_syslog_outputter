2931fd1ef4db3b2c9de5596c7a582c669a742287
nlim <nathaniel.j.lim@gmail.com>
2012-09-08 01:20:02 -0400

    Now its working for papertrail

--- lib/log4r_remote_syslog_outputter/log4r/outputter/remote_syslog_outputter.rb
+++ lib/log4r_remote_syslog_outputter/log4r/outputter/remote_syslog_outputter.rb
@@ -1,16 +1,16 @@
 require 'log4r/outputter/outputter'
 require 'remote_syslog_logger/udp_sender'
+require 'uri'
 
 module Log4r
   class RemoteSyslogOutputter < Log4r::Outputter
     def initialize(name, options = {})
-      hostname, port = options['hostname'], options['port']
+      uri = URI.parse(options['url'])
       options.dup.tap do |o|
         super(name, [:level, :formatter].inject({}) { |h, i| h.tap { |a| a[i] = o.delete(i) } })
-        @udp_sender = RemoteSyslogLogger::UdpSender.new(hostname, port, o)
+        @udp_sender = RemoteSyslogLogger::UdpSender.new(uri.host, uri.port, o.symbolize_keys)
       end
     end
-
     private
     def write(data)
       @udp_sender.write(data)
